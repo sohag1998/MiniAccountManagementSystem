1cc0044d2ab842d98db5ead1a024ec639df02f4c
SOHAG-HOSSAIN\sohag <sohaghossainice@gmail.com>
2025-06-26 21:18:49 +0600

    ServiceCollectionExtensions is added. where all my custom services and Identity configuration is added

---
+++ src/AccountManagemetSystem/AccountManagemetSystem/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,33 @@
+﻿using AccountManagemetSystem.Data;
+using AccountManagemetSystem.Services;
+using Microsoft.AspNetCore.Identity;
+
+namespace AccountManagemetSystem.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddPermissionServices(this IServiceCollection services)
+        {
+            services.AddScoped<IPermissionService, PermissionService>();
+            return services;
+        }
+
+        public static IServiceCollection AddCustomIdentity(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDefaultIdentity<IdentityUser>(options =>
+            {
+
+                options.SignIn.RequireConfirmedAccount = false;
+                options.Password.RequireDigit = false;
+                options.Password.RequiredLength = 6;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequireLowercase = false;
+            })
+            .AddRoles<IdentityRole>()
+            .AddEntityFrameworkStores<ApplicationDbContext>();
+
+            return services;
+        }
+    }
+}
